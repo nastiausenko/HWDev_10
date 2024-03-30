4a2be036fc62ffde9f7fe974f17612232b4a6295
nastiausenko <Usenko.Anastasia@lll.kpi.ua>
2024-03-30 20:25:11 +0200

    init commit

---
+++ src/main/java/org/example/servlets/TimeServlet.java
@@ -0,0 +1,32 @@
+package org.example.servlets;
+
+import java.io.*;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(value = "/time")
+public class TimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        resp.getWriter().write(parseTime(req));
+        resp.getWriter().close();
+    }
+    private String parseTime(HttpServletRequest request) {
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of("UTC"));
+
+        String formattedDate = zonedDateTime.format(dateFormat);
+
+        return "<html>\n" +
+        "<head><title>Current Time</title></head>\n" +
+        "<body>\n" +
+        "<h1>Current Time</h1>\n" +
+        "<p>" + formattedDate + "</p>\n" +
+        "</body></html>";
+    }
+}
